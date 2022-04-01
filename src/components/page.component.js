433e09d01b03f72f365cc624396dc8bb3b88ca19
s-tersh <s.tersh@yandex.ru>
2022-04-01 16:33:30 +0300

    add leader info

--- src/components/page.component.js
+++ src/components/page.component.js
@@ -17,13 +17,18 @@ const Page = ({data, ...props}) => {
             leader ?
             <>
                 <motion.div initial={{'scale': 0, 'opacity': 0}} animate={{'scale': 1, 'opacity': 1}} style={{'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center'}}>
-                    <div style={{'position': 'relative', 'display': 'flex', 'justifyContent': 'center'}}>
-                        <img alt="leader-avatar" src={leader.avatar || `${process.env.PUBLIC_URL}/images/avatar.svg`} style={{'position': 'absolute', 'width': 160, 'height': 160, 'bottom': 75, 'border': '20px solid var(--color-leader)', 'borderRadius': '50%', 'objectFit': 'cover'}}/>
-                        <LeaderShield fill="var(--color-leader)" />
-                    </div>
                     <p className="leader-status">Leader</p>
+                    <div style={{'position': 'relative', 'display': 'flex', 'justifyContent': 'center'}}>
+                        <img alt="leader-avatar" src={leader.avatar || `${process.env.PUBLIC_URL}/images/avatar.svg`} style={{'position': 'absolute', 'width': 130, 'height': 130, 'bottom': 65, 'border': '15px solid var(--color-leader)', 'borderRadius': '50%', 'objectFit': 'cover'}}/>
+                        <LeaderShield fill="var(--color-leader)" width={250} height={250} />
+                    </div>
+                    <p className="leader-club">{leader.total} баллов</p>
                     <p className="leader-fullname">{leader.name} {leader.lastname}</p>
                     <p className="leader-club">{leader.city} {leader.club ? `, «${leader.club}»` : ''}</p>
+                    <p className="leader-events">{leader.events[0].res_1} кг / {leader.events[0].res_2} кг</p>
+                    <p className="leader-events">{leader.events[1].res_1} / {leader.events[1].res_2} повторений</p>
+                    <p className="leader-events">{leader.events[2].res_1} повторений</p>
+                    <p className="leader-events">{leader.events[3].res_1} / {leader.events[3].res_2} повторений</p>
                 </motion.div>
                 <List leader athletes={data.slice(1)} />
             </>
